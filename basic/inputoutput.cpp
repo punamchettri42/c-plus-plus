069c87be50b8572560a723f42bbdf3ee4d9697bf
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-09 12:44:24 +0545

    Add files via upload

---
+++ basic/inputoutput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[50];
+    char post[20];
+    cout << " enter the name";
+    cin>> name;
+    cout << "enter post";
+    cin>> post;
+    cout<< "name:" <<name;
+    cout<<"\n post:" <<post;
+    return 0;
+}
