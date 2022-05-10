64d2b794a29606a98ef0a3b74db7d8dc66c433c5
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-10 07:36:14 +0545

    Add files via upload

---
+++ basic/printnumber.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int number;
+  cout << "Enter an Integer: ";
+  cin >> number;
+  cout << "Entered Integer is: " << number ;//<< endl; //here endl can be written and not written both
+  return 0;
+}
