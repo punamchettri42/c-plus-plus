2b58cdfdca29ebb8cd97fe6b99ca8067be976fc7
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-10 07:41:28 +0545

    Add files via upload

---
+++ basic/arithmetic.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  double num1, num2; // declare variables
+  // take input from end-user
+  cout << "Enter two Numbers :: ";
+  cin >> num1 >> num2;
+
+  // addition of two number
+  cout << num1 << "+" << num2 << " = "<< num1+num2 << endl;
+
+  // subtraction of two number
+  cout << num1 << "-" << num2 << " = "<< num1-num2 << endl;
+
+  // multiplication of two number
+  cout << num1 << "*" << num2 << " = "<< num1*num2 << endl;
+
+  // division of two number
+  cout << num1 << "/" << num2 << " = "<< num1/num2 << endl;
+
+  return 0;
+}
