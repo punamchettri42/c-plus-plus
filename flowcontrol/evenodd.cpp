f72ef61824a9dfadec83d2dbf88ddb370b2e8b54
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-10 08:02:09 +0545

    Add files via upload

---
+++ flowcontrol/evenodd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  
+  int number;
+  cout << "Enter an Integer: " ;
+  cin >> number;
+
+  if (number % 2 == 0)
+    cout << number  << " is Even Number" << endl;
+  else
+    cout << number  << " is Odd Number" << endl;
+
+  return 0;
+}
