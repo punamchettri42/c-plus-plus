eeadb360a77d42daecdecdbcc0b28a605075d288
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-10 07:50:47 +0545

    Add files via upload

---
+++ basic/findingasciivalue.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  char ch;
+
+  cout << "Enter any character : ";
+  cin >> ch;
+
+  cout << "ASCII value is : " << int(ch) << endl;
+
+  return 0;
+}
