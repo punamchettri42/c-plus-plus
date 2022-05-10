6d20a5506e2fed69f8523b2d763367768e1d2027
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-10 07:42:16 +0545

    Add files via upload

---
+++ basic/addnumbers.cpp
@@ -0,0 +1,13 @@
+// C++ program to add two numbers
+#include<iostream>
+using namespace std;
+int main()
+{    
+    double num1, num2;
+    double sum;
+    cout << "Enter Two Numbers : ";
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
