5b9cadfee3358001c04134b82aa108337131445c
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-13 06:45:43 +0545

    Add files via upload

---
+++ basic/namedisplay.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hellow!my name is punam.";
+    return 0;
+}
+
+// we can write comment this way
+/* we can write multi line comment this way as well */
