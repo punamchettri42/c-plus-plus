c394e2597cf48ddd516b080014372654f860b742
punamchettri42 <104554147+punamchettri42@users.noreply.github.com>
2022-05-14 07:27:37 +0545

    Add files via upload

---
+++ basic/feet_ to_inches.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+int main() 
+{
+   int f, i = 0;
+   cout << "Simple C++ Program : Convert Feet to Inches\n";
+   cout << "\nEnter feet : ";
+   cin>>f;
+   i = f * 12;
+   cout << "\n\nTotal Inches : " << i;
+   return 0;
+}
